3c5a8256439db1cc95ff804bc312c3d152c4565c
lydia-l3 <122927604+lydia-l3@users.noreply.github.com>
2024-04-30 20:26:42 +0000

    commit 2 - revising movies form

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-    get("/", { :controller => "misc", :action => "homepage" })
-    get("/directors/youngest", { :controller => "directors", :action => "max_dob" })
-    get("/directors/eldest", { :controller => "directors", :action => "min_dob" })
-    get("/directors", { :controller => "directors", :action => "index" })
-    get("/directors/:path_id", { :controller => "directors", :action => "show" })
-    get("/movies", { :controller => "movies", :action => "index" })
-    get("/movies/:path_id", { :controller => "movies", :action => "show" })
-    post("/insert_movie_record", { :controller => "movies", :action => "create" })
-    get("/delete_movie/:an_id", { :controller => "movies", :action => "destroy" })
-    post("/modify_movie_record/:the_id", { :controller => "movies", :action => "update" })
+  get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors/youngest", { :controller => "directors", :action => "max_dob" })
+  get("/directors/eldest", { :controller => "directors", :action => "min_dob" })
+
+  get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/:path_id", { :controller => "directors", :action => "show" })
+
+  get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  post("/insert_movie_record", { :controller => "movies", :action => "create" })
+  get("/delete_movie/:an_id", { :controller => "movies", :action => "destroy" })
+  post("/modify_movie_record/:the_id", { :controller => "movies", :action => "update" })
   
-  
-  
-    get("/actors", { :controller => "actors", :action => "index" })
-    get("/actors/:path_id", { :controller => "actors", :action => "show" })
-  end
+  get("/actors", { :controller => "actors", :action => "index" })
+  get("/actors/:path_id", { :controller => "actors", :action => "show" })
+end
